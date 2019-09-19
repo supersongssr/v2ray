1dd5f8b4740f938b96143f69b1be016923dc790d
supersongssr <34210429+supersongssr@users.noreply.github.com>
2019-09-19 15:34:01 +0800

    默认选择安装1

--- install.sh
+++ install.sh
@@ -1125,7 +1125,7 @@ while :; do
 		echo -e "$yellow 温馨提示.. 本地安装已启用 ..$none"
 		echo
 	fi
-	read -p "$(echo -e "请选择 [${magenta}1-2$none]:")" choose
+	read -p "$(echo -e "请选择 [${magenta}1-2 默认选择1$none]:")" choose
 	[[ -z $choose ]] && choose="1"
 	case $choose in
 	1)
